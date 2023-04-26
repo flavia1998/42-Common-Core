d3db6270209d612abec02471109227f16fce764b
flavia1998 <flaviapinto6@gmail.com>
2023-04-26 17:56:31 +0100

    update

--- Course/Libft/funcoes/ft_split.c
+++ Course/Libft/funcoes/ft_split.c
@@ -1,43 +1,67 @@
-
 #include "libft.h"
 
-int    count_delimiter(char const *s, char c)
+int row_number(char const *s, char c)
 {
-    char    *str;
-    int     i;
-    int     j;
-
-    str = (char *)s;
-    i = 0;
-    while (str[i] != '\0')
+    int i = 0;
+    int j = 0;
+    while (s[i] != '\0')
     {
-        if (str[i] == c)
-        { 
+        if (s[i] != c && (i == 0 || s[i - 1] == c))
+        {
             j++;
         }
-       i++;
+        i++;
     }
-    return (j);
+    return j;
 }
 
-char    **ft_split(char const *s, char c)
+char **ft_split(char const *s, char c)
 {
-    int     delimiter_count;
-    char    *delimited_str;
-  
-    delimiter_count = count_delimiter(s,c);
-    delimited_str = (char *)malloc( (delimiter_count * sizeof(char)) + 2);
-    if (delimited_str == NULL)
+    char **split_str;
+    int i = 0;
+	int start = 0;
+	int end  = 0;
+    int row_num = row_number(s, c);
+    
+    split_str = (char **)malloc((row_num + 1) * sizeof(char *));
+    if (split_str == NULL)
         return NULL;
-    ft_strlcpy(new_str,str,b_local);
-    return new_str;
+    
+    while (i < row_num)
+    {
+		// ignore delimiter at begining
+        while (s[end] == c)
+            end++;
+
+		start = end;
+
+		// calculate end
+        while (s[end] != '\0' && s[end] != c)
+            end++;
+
+		// allocate space for delimited str
+        split_str[i] = (char *)malloc((end - start + 1) * sizeof(char));
+        if (split_str[i] == NULL)
+            return NULL;
+
+		// copy delimited str
+		ft_strlcpy(split_str[i], s + start, end - start + 1);
+        i++;
+    }
+    
+    split_str[row_num] = NULL;
+    return split_str;
 }
 
 // int main(void)
 // {
-//     char s[50] = "flavia andreia vieira pinto";
-//     char c = ' ';
-//     char *new_array;
-//     new_array = split_strs(s,c);
-//     printf("%s",new_array);
+//     char *s = "lorem ipsum dolor sit amet";
+//     char **split = ft_split(s, ' ');
+//     int x = 0;
+//     while (split[x] != NULL)
+//     {
+//         printf("%s\n", split[x]);
+//         x++;
+//     }
+//     return 0;
 // }
