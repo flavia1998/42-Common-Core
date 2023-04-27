eb8324551ce079cd15bb616651390587710a5388
Flávia Pinto <118297933+flavia1998@users.noreply.github.com>
2023-04-27 17:02:58 +0100

    Add files via upload

--- Course/Libft/funcoes/ft_itoa.c
+++ Course/Libft/funcoes/ft_itoa.c
@@ -1,46 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvieira- <fvieira-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 14:36:43 by fvieira-          #+#    #+#             */
+/*   Updated: 2023/04/27 16:53:30 by fvieira-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_itoa(int n)
-{
-    char    *str_num;
-    int     size_digit;
-    int     reverse_count;
-    int     temp;
-
-    size_digit = 0;
-    temp = n;
-
-    while (temp != 0)
+long	number_digits(int n)
+{ 
+	long     size_digit;
+	
+	size_digit = 0;
+	if (n < 0) 
+	{
+    	n = -n;
+	}
+    while (n != 0)
     {
         size_digit++;
-        temp = temp / 10; //numero de digitos atraves das vezes que dividimos
-    }
+        n = n / 10; 
+	}
+	size_digit++;
+	return size_digit;
+}
 
-    str_num = (char *)malloc((sizeof(char)) * (size_digit + 1));
-    if (!str_num)
-        return NULL;
+char    *ft_itoa(int n)
+{
+    char	*str_num;
+    long	reverse_count;
+	long	digits;
+	long 	neg;
 
-    reverse_count = size_digit - 1;// desconsiderar o null
-    
-
-    while (reverse_count >= 0)
+	neg = 0;
+	digits = number_digits(n);
+    str_num = (char *)malloc((sizeof(char)) * (digits + 2));
+	if (n < 0)
+	{
+		neg = 1;
+		n *= -1;
+	}
+    reverse_count = digits - 1;
+    while (reverse_count > 0)
     {
-        str_num[reverse_count] = (n % 10) + '0'; // 
+        str_num[reverse_count] = (n % 10) + '0'; 
         n = n / 10;
         reverse_count--;
     }
-    str_num[size_digit] = '\0';
+		if (neg)
+		str_num[reverse_count] = '-';
+    str_num[digits] = '\0';
     return str_num;
 }
 
-// int main(void)
+// int main()
 // {
-//     int n = 20;
+//     int n = -200;
 //     char *num;
 //     num = ft_itoa(n);
 //     printf("%s",num);
 //     return 0;
-
 // }
-
