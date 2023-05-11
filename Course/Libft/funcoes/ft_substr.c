82089f8caafc848c33613f6c61a5aa8dd905c73b
Flavia Andreia Vieira Pinto <fvieira-@c1r6s1.42porto.com>
2023-05-11 16:23:45 +0100

    update

--- Course/Libft/funcoes/ft_substr.c
+++ Course/Libft/funcoes/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fvieira- <fvieira-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 14:59:07 by fvieira-          #+#    #+#             */
-/*   Updated: 2023/04/20 17:45:15 by fvieira-         ###   ########.fr       */
+/*   Updated: 2023/05/11 15:07:01 by fvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	j = 0;
 	if (ft_strlen(s) < start)
 		len = 0;
-	if (ft_strlen(s + start) < len)
+	else if (ft_strlen(s + start) < len)
 		len = ft_strlen(s + start);
 	substr = malloc(sizeof(char) * (len + 1));
 	if (substr == NULL)
@@ -37,7 +37,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 // int main(void)
 // {
 // 	char *substr;
-// 	substr = ft_substr("maria joana", 2, 4);
+// 	substr = ft_substr("hora", 45896215763, 0);
 // 	printf("%s", substr);
 // 	return 0;
 // }
