1d6c968b563da24cc1ccd0130a257de56e39d75c
flavia1998 <flaviapinto6@gmail.com>
2023-11-11 17:57:46 +0000

    push_swap

---
+++ push_swap/main.c
@@ -0,0 +1,15 @@
+#include "push_swap.h"
+
+int main(int argc, char**argv)
+{
+   if (argc != 2 )
+   {
+    return 0;
+   }
+   
+   
+    
+    
+    return 0;
+}
+
---
+++ push_swap/push_swap.h
@@ -0,0 +1,12 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+
+
+
+#endif
