5ae8434af055d000a9e2a13a7193f1f200184ca2
flavia1998 <flaviapinto6@gmail.com>
2024-03-06 22:03:10 +0000

    update

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fvieira- < fvieira-@student.42porto.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 16:37:38 by fvieira-          #+#    #+#             */
-/*   Updated: 2024/02/19 16:57:11 by fvieira-         ###   ########.fr       */
+/*   Updated: 2024/03/06 21:40:19 by fvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ int main(int argc, char **argv)
 		}
 		else
 		{
-			push_start(&stacks->stack_a, number);
+			push_end(stacks->stack_a, number);
 		}
 
 		i++;
